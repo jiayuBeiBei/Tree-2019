bb48b179736c59d369159f3f43327e2e5c11f436
jiayuBeiBei <784030774@qq.com>
2019-03-07 13:40:58 +0800

    二叉排序第一次提交

---
+++ src/com/jiayu/Main.java
@@ -0,0 +1,49 @@
+package com.jiayu;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public class Node{
+        //Node必须是public,不然放不到list等集合框架里
+        private  Node right=null;
+        private  Node left=null;
+        private Node parent=null;
+        private  int val=0;
+        Node (int val){
+            this.val=val;
+        }
+
+        public Node(Node right, Node left, Node parent, int val) {
+            this.right = right;
+            this.left = left;
+            this.parent = parent;
+            this.val = val;
+        }
+        //判断俩个节点想不想等,是一个比较复杂的问题,因为一个节点的子树可能有很多层
+    }
+    Node root=null;
+    public int add(int val){
+        if(root==null){
+            root=new Node(val);
+        }else{
+            //层序查找完全二叉树,返回要加的子节点位置的指针
+            Node last=findBTreeLastNode(root);
+            last=new Node(val);
+        }
+        return 0;
+    }
+    public int remove(){
+        return 0; 
+    }
+
+    public static void main(String[] args) {
+        Main tree=new Main();
+            tree.add(0);
+            tree.add(1);
+            tree.add(2);
+            tree.add(3);
+            tree.remove();
+        System.out.println("Hello World!");
+    }
+}
