6633ec3d36198159fdbee7f73569a29960c42bbd
jiayuBeiBei <784030774@qq.com>
2019-03-13 12:47:04 +0800

    Node从内部类变为正常类

--- src/com/jiayu/Main.java
+++ src/com/jiayu/Main.java
@@ -2,37 +2,21 @@ package com.jiayu;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
-    public class Node{
-        //Node必须是public,不然放不到list等集合框架里
-        private  Node right=null;
-        private  Node left=null;
-        private Node parent=null;
-        private  int val=0;
-        Node (int val){
-            this.val=val;
-        }
 
-        public Node(Node right, Node left, Node parent, int val) {
-            this.right = right;
-            this.left = left;
-            this.parent = parent;
-            this.val = val;
-        }
-        //判断俩个节点想不想等,是一个比较复杂的问题,因为一个节点的子树可能有很多层
-    }
     Node root=null;
 
 
-    private int preOrder(){
+    private int preOrder(Node root){
         List<Node> stack=new LinkedList<>();
-        Node p=this.root;
+        Node p=root;
         while(p!=null||!stack.isEmpty()){
             while(p!=null){
                 System.out.println(p.val);
                 stack.add(p);
-                System.out.println(p.val);
+                //System.out.println(p.val);
                 p=p.left;
             }
             if (!stack.isEmpty()){
@@ -44,13 +28,66 @@ public class Main {
         }
         return 0;
     }
+    private int inOrder(Node root){
+        List<Node> stack=new LinkedList<>();
+        Node p=root;
+        while(p!=null||!stack.isEmpty()){
+            while(p!=null){
+
+                stack.add(p);
+
+                p=p.left;
+            }
+            if (!stack.isEmpty()){
+                Node temp=stack.remove(stack.size()-1);
+                if(temp!=null){System.out.println(temp.val);}
+                p=temp.right;
+
+            }
+
+        }
+        return 0;
+    }
+    //后序遍历的非递归实现
+    public void noRecPostOrder(Node root){
+        Stack<Node> stack=new Stack<Node>();
+        Node node =root;
+        while(root!=null){
+            //左子树入栈
+            for(;root.left!=null;root=root.left){
+                stack.push(root);
+            }
+            //当前结点无右子树或右子树已经输出
+            while(root!=null&&(root.right==null||root.right==node)){
+                //第一种情况 ;当前root节点,左右节点都为null
+                //第二种情况: 当前root节点,右节点为根节点
+                System.out.println(root.val);
+                //纪录上一个已输出结点
+                node =root;
+                if(stack.empty()){
+                    return;}
+                root=stack.pop();
+            }
+            //处理右子树
+            stack.push(root);
+            root=root.right;
+        }
+    }
+
     public static void main(String[] args) {
         Main tree=new Main();
-            tree.add(0);
-            tree.add(1);
-            tree.add(2);
-            tree.add(3);
-            tree.preOrder();
-        System.out.println("Hello World!");
+       Node root=new Node(1);
+       Node croot2=new Node(2);
+       Node croot3=new Node(3);
+       Node croot4=new Node(4);
+       root.setLeft(croot2);
+       root.setRight(croot3);
+       croot3.setLeft(croot4);
+        tree.preOrder(root);
+        System.out.println("---------mid");
+        tree.inOrder(root);
+        System.out.println("---------post");
+        tree.noRecPostOrder(root);
+
     }
 }
---
+++ src/com/jiayu/Node.java
@@ -0,0 +1,53 @@
+package com.jiayu;
+
+public class Node{
+    //Node必须是public,不然放不到list等集合框架里
+      Node right=null;
+      Node left=null;
+    Node parent=null;
+     int val=0;
+    Node (int val){
+        this.val=val;
+    }
+
+    public Node(Node right, Node left, Node parent, int val) {
+        this.right = right;
+        this.left = left;
+        this.parent = parent;
+        this.val = val;
+    }
+    //判断俩个节点想不想等,是一个比较复杂的问题,因为一个节点的子树可能有很多层
+
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
